34d5cff7aa3f324dd29273bb36ccc9eb864d0582
Luke Armitage <gillyobeast@users.noreply.github.com>
2017-03-28 23:14:09 +0100

    figured out how to extract values from the csv
    need to figure out why `stod` won't work, then push_back into s0

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
-
+#include <string>
+#include <fstream>
+#include <sstream>
+#include "Project.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    ifstream data("data.csv"); // creates input stream object 'data'
+    string line, s0i, s1i;
+    vector<double> s0, s1;
+
+    while (getline(data, line)){ //processes 'data' line by line - stores each line in 'line'
+        stringstream lineStream(line); // creates stringstream obj 'lineStream' with contents of 'line'
+        getline(lineStream,s0i,',' ); // separates 'line' into 's0i' and 's1i'
+        getline(lineStream,s1i,',' );
+        cout <<"s0i:" << s0i << ", s1i:" << s1i << endl; // do processing of 'line' in 'data' here
+
+        //do i want to figure out how to put s0i, s1i in a vector before processing? probably.
+        s0.push_back(stod(s0i);
+
+    }
+
 }
