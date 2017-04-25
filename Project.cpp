590e03a3b670c4cc8f416cbe4266f065555a5b46
Luke Armitage <gillyobeast@users.noreply.github.com>
2017-04-25 01:11:43 +0100

    add payoff classes

--- Project.cpp
+++ Project.cpp
@@ -8,10 +8,37 @@
 using namespace std;
 
 
-class spreadCall: public Payoff{
+class spreadCall: public Payoff{ //todo: move to a 'payoff.h' header
 public:
     double Value(vector<double> S){
-        return 0;
+        if(S[0]-S[1]-K>0) return S[0]-S[1]-K;
+        else return 0;
+    }
+    void set_K(double _K){K=_K;}
+private:
+    double K; // strike price
+};
+
+class minCall: public Payoff{
+public:
+    double Value(vector<double> S){
+        double S_min = S[0];
+        if(S[0]>S[1]) S_min = S[1];
+        if(S_min-K>0) return S_min-K;
+        else return 0;
+    }
+    void set_K(double _K){K=_K;}
+private:
+    double K; // strike price
+};
+
+class maxCall: public Payoff{
+public:
+    double Value(vector<double> S){
+        double S_max = S[0];
+        if(S[0]<S[1]) S_max = S[1];
+        if(S_max-K>0) return S_max-K;
+        else return 0;
     }
     void set_K(double _K){K=_K;}
 private:
